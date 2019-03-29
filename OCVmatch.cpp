c79e0e28655e58e7f43f634b69b8ea7e848aea55
Patrick Skinner <patrickskinner@outlook.com>
2019-03-29 14:48:00 +1300

    Add files via upload

---
+++ OCVmatch.cpp
@@ -0,0 +1,56 @@
+// OCVmatch.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/calib3d.hpp>
+
+#include <iostream>
+#include <stdio.h>
+
+using namespace cv;
+using namespace std;
+
+extern "C" int __declspec(dllexport) __stdcall  Init()
+{
+	// Open console for DLL debugging
+	FILE * pConsole;
+	AllocConsole();
+	freopen_s(&pConsole, "CONOUT$", "wb", stdout);
+
+	return 0;
+}
+
+extern "C" void __declspec(dllexport) __stdcall ComputePNP(Vec3f *&op, Vec2f *&ip, unsigned char *&rv, unsigned char *&tv)
+{
+	Mat imagePoints;
+	Mat objectPoints;
+	Mat cameraMatrix;
+	Mat distCoeffs;
+
+	Mat rotation;
+	Mat translation;
+
+	
+	imagePoints = Mat(4, 2, CV_32F, ip);
+	objectPoints = Mat(4, 3, CV_32F, op);
+
+	cout << "imagePoints = " << endl << " " << imagePoints << endl << endl;
+	cout << "objectPoints = " << endl << " " << objectPoints << endl << endl;
+
+	cameraMatrix = (Mat_<float>(3, 3) << 1514, 0, 1514/2, 0, 1514, 795/2, 0, 0, 1);
+	
+	solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs, rotation, translation);
+
+	Mat rotationMat;
+	Rodrigues(rotation, rotationMat);
+	cout << "rotationMat = " << endl << " " << rotationMat << endl << endl;
+
+	memcpy(rv, rotationMat.data, rotationMat.total() * rotationMat.elemSize());
+	memcpy(tv, translation.data, translation.total() * translation.elemSize());
+}
+
---
+++ SolvePNP.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using Vuforia;
+
+public class input : MonoBehaviour
+{
+    Vector2[] points = new Vector2[4];
+    int i = 0;
+    public GameObject ground;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Screen Size: " + Screen.width + "x" + Screen.height);
+
+        OpenCVInterop.Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+
+        {
+            Vector2 mousePos = new Vector2();
+            mousePos = Input.mousePosition;
+
+            if(i < 4)
+            {
+                points[i] = mousePos;
+                i++;
+                Debug.Log(mousePos);
+            }
+            else if (i == 4)
+            {
+                Vector3[] vertices = new Vector3[4];
+
+                for (int i = 0; i < 4; i++) vertices[i] = ground.transform.TransformPoint(ground.GetComponent<MeshFilter>().mesh.vertices[i]); //Get world positions of vertices
+                Debug.Log("Vertices: " + vertices[0] + ", " + vertices[1] + ", " + vertices[2] + ", " + vertices[3]);
+
+                float[] rotationMatrix = new float[9];
+                float[] translationMatrix = new float[3];
+                Matrix4x4 transformationMatrix = new Matrix4x4();
+                
+                OpenCVInterop.ComputePNP(ref vertices, ref points, ref rotationMatrix, ref translationMatrix);
+
+                transformationMatrix[0, 0] = rotationMatrix[0];
+                transformationMatrix[0, 1] = rotationMatrix[1];
+                transformationMatrix[0, 2] = rotationMatrix[2];
+                transformationMatrix[0, 3] = translationMatrix[0];
+
+                transformationMatrix[1, 0] = rotationMatrix[3];
+                transformationMatrix[1, 1] = rotationMatrix[4];
+                transformationMatrix[1, 2] = rotationMatrix[5];
+                transformationMatrix[1, 3] = translationMatrix[1];
+
+                transformationMatrix[2, 0] = rotationMatrix[6];
+                transformationMatrix[2, 1] = rotationMatrix[7];
+                transformationMatrix[2, 2] = rotationMatrix[8];
+                transformationMatrix[2, 3] = translationMatrix[2];
+
+                transformationMatrix[3, 3] = 1;
+
+				//Convert from OpenCV to Unity coordinates
+                Matrix4x4 invertYM = Matrix4x4.TRS(Vector3.zero, Quaternion.identity, new Vector3(1, -1, 1));
+                
+                transformationMatrix = transformationMatrix * invertYM;
+                //transformationMatrix = transformationMatrix * this.gameObject.transform.localToWorldMatrix;
+                Debug.Log(transformationMatrix.ToString());
+
+                ground.transform.position = transformationMatrix.MultiplyPoint3x4(ground.transform.position);
+                ground.transform.rotation *= Quaternion.LookRotation(transformationMatrix.GetColumn(2), transformationMatrix.GetColumn(1));
+
+                ground.GetComponent<MeshRenderer>().enabled = true;
+
+
+            }
+        }
+    }
+}
+
+
+internal static class OpenCVInterop
+{
+    [DllImport("OCVmatch")]
+    internal static extern int Init();
+
+    [DllImport("OCVmatch")]
+    internal static extern int Close();
+
+    [DllImport("OCVmatch")]
+    internal static extern int GrabImage(ref byte[] image, ref int width, ref int height);
+
+    [DllImport("OCVmatch")]
+    internal static extern void ComputePNP(ref Vector3[] op, ref Vector2[] ip, ref float[] rv, ref float[] tv);
+}
+
